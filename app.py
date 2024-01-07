16cb672222cd14a02c47d11f8a673e2e7ff530e8
thruway22 <121792615+thruway22@users.noreply.github.com>
2024-01-07 14:19:57 +0300

    u

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.title('Test')
+
+info = pd.read_csv('info.csv')
+
+nav1 = pd.read_csv('nav1.csv')
+nav2 = pd.read_csv('nav2.csv')
+nav3 = pd.read_csv('nav3.csv')
+nav = pd.concat([nav1, nav2, nav3])
+
+fund = st.selectbox(info[['Fund Name']])
+
+st.plot(nav[nav['name'] == fund].nav.plot())
