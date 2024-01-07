1bb0a70d28643cbdb7758fc4e0deb82553ef7cc5
thruway22 <121792615+thruway22@users.noreply.github.com>
2024-01-07 14:52:00 +0300

    u

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ nav = nav.set_index('date')
 fund = st.selectbox('fund', info[['Fund Name']])
 
 if fund:
-    df = nav[nav['name'] = fund]
+    df = nav[nav['name'] == fund]
     fig = plt.plot(df['nav'])
     st.pyplot(fig)
