3a9a7071c62e471a86c660e0bbbb64cf058e1868
thruway22 <121792615+thruway22@users.noreply.github.com>
2024-01-07 14:33:24 +0300

    u

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title('Test')
 
@@ -13,4 +14,4 @@ nav = pd.concat([nav1, nav2, nav3])
 fund = st.selectbox('fund', info[['Fund Name']])
 
 if fund:
-    st.pyplot(nav[nav['name'] == fund][['NAV (SAR)']].plot())
+    st.pyplot(nav[nav['name'] == fund][['NAV (SAR)']])
